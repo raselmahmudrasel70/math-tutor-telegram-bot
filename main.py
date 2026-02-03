bdd7fbcf6052374092f093c78caec621087eec40
raselmahmudrasel70 <raselmahmudrasel70@gmail.com>
2026-02-03 15:16:12 +0600

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from math_bot import main
+
+if __name__ == "__main__":
+    main()
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     ApplicationBuilder,
